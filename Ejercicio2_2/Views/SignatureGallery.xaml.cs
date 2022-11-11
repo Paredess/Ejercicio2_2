c90741b1744385d85825e87aee8ba4aa307c5116
Daniel Paredes <estudiantesuth56@gmail.com>
2022-11-10 22:47:28 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2_2/Views/SignatureGallery.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Ejercicio2_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio2_2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignatureGallery : ContentPage
+    {
+        public SignatureGallery()
+        {
+            InitializeComponent();            
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            fillCollectionView();
+        }
+
+        private async void fillCollectionView()
+        {
+            clvFirmas.ItemsSource = await App.DBase.obtenerFirmas();
+        }
+
+        private async void swpDelete_Invoked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Confirmar", "Deliminar esta firma?", "Sí", "No"))
+            {
+                var firma = (Firma)(sender as SwipeItemView).CommandParameter;
+                var result = await App.DBase.borrarFirma(firma);
+
+                if (result == 1)
+                {
+                    fillCollectionView();
+                    await DisplayAlert("Aviso", "Firma Eliminada", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Aviso", "Ha ocurrido un error", "OK");
+                }
+            }
+        }
+
+        /*private async void swpUpdate_Invoked(object sender, EventArgs e)
+        {
+            var firma = (Firma)(sender as SwipeItemView).CommandParameter;
+
+            MainPage page = new MainPage();
+            page.BindingContext = firma;
+            await Navigation.PushAsync(page);
+        }*/
+    }
+}
